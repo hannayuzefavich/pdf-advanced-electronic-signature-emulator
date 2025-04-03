665b24040457a1903f04f8eb0546ecfb608ee579
hannayuzefavich <juzeffch@gmail.com>
2025-04-03 21:41:14 +0200

    Comments added

--- auxillary/main.py
+++ auxillary/main.py
@@ -9,10 +9,17 @@ class KeyGenerator(ttk.Frame):
         super().__init__(container)
 
         self.generate_button = tk.Button(self, text='Generate key pair', command=self.begin_setup, width=30, relief='raised', height=10)
+        #nie moge wycentrowac tego przycisku xd
         self.generate_button.grid(column=3, row=3, columnspan=3, sticky=tk.NSEW)
 
         self.grid(padx=10, pady=10, sticky=tk.NSEW)
-
+    #tutaj chce zrobic takiego wizarda w osobnym oknie, z krokami:
+    # 1.podanie sciezki do zapisania na dysku klucza publicznego
+    # 2.podanie pina do zaszyfrowania klucza prywatnego
+    # 3.po zweryfikowaniu, czy pola zostały wypełnione, wywołac funkcję generate() -> rsa.newkeys(4096), zapisanie klucza publicznego
+    # na dysku, zaszyfrowanie prywatnego
+    # 4. zamknięcie okna dodatkowego - "wizarda"
+    # 5. messagebox że klucze wygenerowane
     def begin_setup(self):
         self.setup_window = tk.Toplevel(self)
         self.setup_window.title('Setup')
