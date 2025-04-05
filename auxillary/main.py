09b76f783bd6144db68cb384844d1d9407feee83
Szymon <szymonlis21@gmail.com>
2025-04-05 16:46:49 +0200

    add field for selecting file location

--- auxillary/main.py
+++ auxillary/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showerror
+from tkinter import filedialog
 import rsa
 
 
@@ -40,31 +41,46 @@ class KeyGenerator(ttk.Frame):
 
         for i in range(5):
             self.grid_rowconfigure(i, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
 
         # add path label
         label = tk.Label(self.setup_window, text="location where public key will be stored: ")
-        label.grid(row=0, column=1)
+        label.grid(row=0, column=0)
 
         # add path input field
-        path = tk.Entry(self.setup_window)
-        path.grid(row=1, column=1)
+        self.path = tk.Entry(self.setup_window, state="readonly")
+        self.path.grid(row=1, column=0)
+        browse_button = tk.Button(self.setup_window, text="Przeglądaj", command=self.browse_file)
+        browse_button.grid(row=1, column=1)
 
         # add pin label
         label = tk.Label(self.setup_window, text="input your pin: ")
-        label.grid(row=2, column=1)
+        label.grid(row=2, column=0)
 
         # add pin input field
         code = tk.Entry(self.setup_window, show="•")
-        code.grid(row=3, column=1)
+        code.grid(row=3, column=0)
 
         # add button that generate rsa keys
         btn = tk.Button(self.setup_window, text='Generate', command=self.generate, relief='raised')
-        btn.grid(row=4, column=1, sticky=tk.NSEW)
+        btn.grid(row=4, column=0, sticky=tk.NSEW)
 
     def generate(self):
         #rsa.newkeys(4096)
         pass
 
+    def browse_file(self):
+        file_path = filedialog.asksaveasfilename(
+            title="Zapisz plik",
+        )
+
+        #insert selected path into input field
+        if file_path:
+            self.path.config(state="normal")
+            self.path.delete(0, tk.END)
+            self.path.insert(0, file_path)
+            self.path.config(state="readonly")
 
 class App(tk.Tk):
     def __init__(self):
