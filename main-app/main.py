d3e7f8f5d7a4827c067ee0294b7e729acc54661d
Szymon <szymonlis21@gmail.com>
2025-04-06 15:47:12 +0200

    add functionality of browsing pdf files

---
+++ main-app/main.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showerror
+from tkinter import filedialog
+
+
+class PADES(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        self.grid(row=0, column=0, sticky=tk.NSEW)
+
+        # grid configuration for centering button
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        # add path input field
+        self.path = tk.Entry(self, state="readonly")
+        self.path.grid(row=1, column=0)
+        browse_button = tk.Button(self, text="Przeglądaj", command=self.browse_file)
+        browse_button.grid(row=1, column=1)
+
+    def browse_file(self):
+        file_path = filedialog.askopenfile(
+            title="wybierz plik",
+            filetypes=[("Pliki PDF", "*.pdf")]
+        )
+
+        #insert selected path into input field
+        if file_path:
+            self.path.config(state="normal")
+            self.path.delete(0, tk.END)
+            self.path.insert(0, file_path.name)
+            self.path.config(state="readonly")
+
+
+class mainApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Key generator')
+        self.geometry('400x300')
+        self.resizable(False, False)
+
+
+if __name__ == "__main__":
+    app = mainApp()
+    PADES(app)
+    app.mainloop()
